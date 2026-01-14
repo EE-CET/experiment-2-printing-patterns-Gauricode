5d14123d0a3a4d1d350ff4ecebf37532156beb06
Gauricode <71497617+Gauricode@users.noreply.github.com>
2026-01-14 22:54:13 +0530

    Update PatternPrinting.java

--- src/PatternPrinting.java
+++ src/PatternPrinting.java
@@ -1,16 +1,4 @@
-import java.util.Scanner;
 
 public class PatternPrinting {
-    public static void main(String args[]){
-        System.out.println("Enter the number of stars to print in a line:");
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        for(int i=0;i<n;i++){
-            System.out.print("* ");
-        }
-        System.out.println("");
-        for(int i=0;i<n;i++){
-            System.out.println("*");
-        }
-    }
+   
 }
